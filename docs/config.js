11352ef1adba652b0ee69a4732ee7d051c7bfe41
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2021-04-26 19:33:26 +0900

    update

--- docs/config.js
+++ docs/config.js
@@ -3,36 +3,10 @@ const config = {
   "style": "https://optgeo.github.io/fgd-tokio/style.json",
   "chapters": [
     {
-      "title": "母島の沖港",
-      "hash": "15.77/26.641922/142.160547/0/56",
-      "description": "<p>母島の南西部に位置する沖港は、母島と父島を結ぶ海上交通の結節点となっています。</p> <p>490トンの定期船「ははじま丸」、貨物船が利用する-4.5m岸壁２バース、 漁船用の船揚場、物揚場、防波堤、船客待合所、港湾緑地施設（脇浜なぎさ公園） 等あるそうです。</p>ソース：<a href='https://www.soumu.metro.tokyo.lg.jp/07ogasawara/island_resident/harbor.html#harbor3'>東京都小笠原支庁</a>",
-      "id": "chapter-1",
-      "alignment": "right",
-      "callback": null,
-      "hidden": false,
-      "mapAnimation": "flyTo",
-      "rotateAnimation": true,
-      "onChapterEnter": [
-
-      ],
-      "onChapterExit": [
-
-      ],
-      "location": {
-        "zoom": 15.77,
-        "center": [
-          142.160547,
-          26.641922
-        ],
-        "bearing": 0.0,
-        "pitch": 56.0
-      }
-    },
-    {
       "title": "迎賓館赤坂離宮",
       "hash": "18.19/35.680584/139.728617/135.6/70",
       "description": "<p>迎賓館赤坂離宮は、明治42年に東宮御所として建設された、 日本では唯一のネオ・バロック様式による宮殿建築物です。</p> <p>第２次世界大戦の後、10数年を経て日本が国際社会へ復帰し、 外国からの賓客を迎えることが多くなったため、 国の迎賓施設へと大規模な改修を施し、和風別館の新設と合わせて 昭和49年に現在の迎賓館として新たな歩みを始め、現在に至っています。</p> <p>その後、平成21年に行われた大規模改修工事の後には、 日本の建築を代表するものの一つとして、国宝に指定されました。</p> <p>これまで多くの国王、大統領、首相などをお迎えしたほか、 主要国首脳会議などの国際会議の場としても使用されています。</p> ソース：<a href='https://www.geihinkan.go.jp/akasaka/'>迎賓館赤坂離宮</a>",
-      "id": "chapter-2",
+      "id": "chapter-1",
       "alignment": "right",
       "callback": null,
       "hidden": false,
@@ -52,6 +26,32 @@ const config = {
         ],
         "bearing": 135.6,
         "pitch": 70.0
+      }
+    },
+    {
+      "title": "母島の沖港",
+      "hash": "15.77/26.641922/142.160547/0/56",
+      "description": "<p>母島の南西部に位置する沖港は、母島と父島を結ぶ海上交通の結節点となっています。</p> <p>490トンの定期船「ははじま丸」、貨物船が利用する-4.5m岸壁２バース、 漁船用の船揚場、物揚場、防波堤、船客待合所、港湾緑地施設（脇浜なぎさ公園） 等あるそうです。</p>ソース：<a href='https://www.soumu.metro.tokyo.lg.jp/07ogasawara/island_resident/harbor.html#harbor3'>東京都小笠原支庁</a>",
+      "id": "chapter-2",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 15.77,
+        "center": [
+          142.160547,
+          26.641922
+        ],
+        "bearing": 0.0,
+        "pitch": 56.0
       }
     }
   ],
